eba0ab06af4c344d0484706e025cc80d7a352f6d
Bintangr20 <breihandani@gmail.com>
2023-11-08 11:34:35 +0700

    perbaikan route produk

--- routes/produk.js
+++ routes/produk.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 // Endpoint untuk mencari produk berdasarkan ID
-router.get('/products/:id', productController.getProductsById);
+router.get('/produk/:id', productController.getProductsById);
 
 // Endpoint untuk mendapatkan semua produk
-router.get('/products', productController.getProducts);
+router.get('/produk', productController.getProducts);
 
 module.exports = router;
