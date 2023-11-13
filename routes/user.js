4355873ad0fcb657f087c007d3a24169a3fdd4e7
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-11-13 09:28:26 +0700

    add get user router

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user');
 
+router.get('/user', userController.getAllUser);
+router.get('/user/:username', userController.getUsername);
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 
