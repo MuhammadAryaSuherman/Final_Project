ccaf7033f9305ff9a97c943960303c0e9fbe39bb
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-11-20 16:49:10 +0700

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controller/user');
 
 router.get('/user', userController.getAllUser);
-router.get('/user/:username', userController.getUsername);
+router.get('/user/:id', userController.getUserbyid);
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 
