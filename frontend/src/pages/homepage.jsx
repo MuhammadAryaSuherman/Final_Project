21d6cccc31ea6aad1d79d6cbaf1b0a82fb40c511
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-11-27 18:08:18 +0700

    Update homepage.jsx

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,10 +1,18 @@
-import { HStack, VStack, useBreakpointValue } from "@chakra-ui/react";
+import { HStack, VStack, useBreakpointValue, Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Product from "../component/products";
 import { getAllProduct } from "../modules/fetch";
+import { useLocation } from "react-router-dom";
 
 export default function Homepage() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  // Get the page query parameter from the URL
+  const location = useLocation();
+  const pageQueryParam = new URLSearchParams(location.search).get("page");
+  const initialPage = pageQueryParam ? parseInt(pageQueryParam, 10) : 1;
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -17,68 +25,61 @@ export default function Homepage() {
     };
 
     fetchProducts();
-  }, []);
+    setCurrentPage(initialPage); // Set the initial page
+  }, [initialPage]);
 
-  const itemsPerRow = 5;
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const visibleProducts = products.slice(startIndex, endIndex);
 
-  const chunkArray = (arr, size) => {
-    const chunkedArr = [];
-    for (let i = 0; i < arr.length; i += size) {
-      chunkedArr.push(arr.slice(i, i + size));
-    }
-    return chunkedArr;
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
   };
 
-  const chunkedProducts = chunkArray(products, itemsPerRow);
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
   const isSmallScreen = useBreakpointValue({ base: true, md: false });
 
   return (
-    <>
-
+    <VStack w="100%" spacing="10" justifyContent="center" mt="20px">
       {isSmallScreen ? (
-        <VStack w="100%" spacing="10" justifyContent="center" mt="20px">
-          {products.map((product) => (
-            <Product
-              key={product.id}
-              {...product}
-              flexBasis="100%"
-              textAlign="center"
-              my="10px"
-            />
-          ))}
-        </VStack>
+        visibleProducts.map((product) => (
+          <Product
+            key={product.id}
+            {...product}
+            flexBasis="100%"
+            textAlign="center"
+            my="10px"
+          />
+        ))
       ) : (
-        <HStack
-          w="90%"
-          spacing="10"
-          justifyContent="center"
-          flexWrap="wrap"
-          mt="20px"
-        >
-          {chunkedProducts.map((rowProducts, rowIndex) => (
-            <HStack
-              key={rowIndex}
-              w="100%"
-              justifyContent="space-around"
-              flex="0 1 auto"
-            >
-              {rowProducts.map((product) => (
-                <Product
-                  key={product.id}
-                  {...product}
-                  flexBasis="18%"
-                  flexGrow="0"
-                  flexShrink="0"
-                  textAlign="center"
-                  mx="2%"
-                  my="10px"
-                />
-              ))}
-            </HStack>
-          ))}
-        </HStack>
+        <>
+          <HStack w="100%" spacing="10" justifyContent="center" flexWrap="wrap">
+            {visibleProducts.map((product) => (
+              <Product
+                key={product.id}
+                {...product}
+                flexBasis={`calc(33.33% - 20px)`}
+                flexGrow="0"
+                flexShrink="0"
+                textAlign="center"
+                my="10px"
+              />
+            ))}
+          </HStack>
+        </>
       )}
-    </>
+      <HStack mt="20px">
+        <Button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </Button>
+        <Button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </Button>
+      </HStack>
+    </VStack>
   );
 }
