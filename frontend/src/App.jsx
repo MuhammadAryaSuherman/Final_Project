f1b6ebe8bf168042c161dfe062df228678079cdd
Bintangr20 <breihandani@gmail.com>
2023-11-27 21:36:47 +0700

    delete reviewPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Orderpage from "./pages/orderPage";
 import UserProfile from "./pages/UserProfile";
-import ReviewPage from "./pages/reviewPage";
 import GamePage from "./pages/gamepage";
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Routes>
             <Route path= {"/homepage"} element={<Homepage/>}/>
             <Route path= {"/products/:id"} element= {<Orderpage/>}/>
-            <Route path= {"/products/:id/reviews"} element= {<ReviewPage/>}/>
             <Route path= {"/user/:id"} element={<UserProfile />} />
             <Route path= {"/login"} element={<Login />}/>
             <Route path= {"/register"} element={<Register />}/>
